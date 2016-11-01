fe1b09f619be9844fbb58e35e8d18f191e34c259
Tudor Popescu <Tudor Popescu>
2016-11-01 23:03:57 +0200

    Remove registration component

--- src/main.js
+++ src/main.js
@@ -1,10 +1,8 @@
 import Vue from 'vue'
 
 import hypeForm from './HypeStepForm.vue'
-import registration from './registration.vue'
 
 Vue.component('hype-form-container',hypeForm)
-Vue.component('reg-form',registration)
 
 new Vue({
     el: '#app',
